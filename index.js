8215111fa84945eaf5149a7c926b6ce7ea68a165
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-05-21 14:46:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var recipes = { siga: "2 kilo" }
 
 
 function updateObjectWithKeyAndValue(object, key, value){
-  // object[key] = value;
-    // return object
+  object[key] = value;
+    
   
   Object.assign({}, object, { key: value})
   
     return object;
-} return object; 
+} 
