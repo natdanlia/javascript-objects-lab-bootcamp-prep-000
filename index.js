3b587190ad7422a6754447abdb0d17a15e4d3e6d
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-05-21 14:54:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   // object[key] = value;
     
   
-  Object.assign({}, object, { key: value})
+  Object.assign(object, { key: value})
   
     return object;
 } 
