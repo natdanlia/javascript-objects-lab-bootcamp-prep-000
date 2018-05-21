640717a01c7be05fe8749c1353921ef7ca0203e9
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-05-21 14:50:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function updateObjectWithKeyAndValue(object, key, value){
   
     return object;
 } 
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object
+}
