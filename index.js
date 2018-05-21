d6b6f223ede35a029476e1fa1644b8f7d7ff2d11
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-05-21 15:30:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function destructivelyDeleteFromObjectByKey(object, key){
 }
  function deleteFromObjectByKey(object, key){
   var newObject = Object.assign({}, object) 
-    // delete newObject.key 
+    delete newObject.key 
     return newObject;
  }
 
